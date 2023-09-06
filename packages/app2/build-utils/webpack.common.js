c02f4c7ad19a5f36c0d19ab78c5205e9958e49cd
Asif Vora <asifvora@SF-LAP-103.local>
2023-09-06 11:12:05 +0530

    feat: Setup for App1 and App2 using webpack and configurations

---
+++ packages/app2/build-utils/webpack.common.js
@@ -0,0 +1,69 @@
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const { ModuleFederationPlugin } = require("webpack").container;
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HotModuleReplacementPlugin = require("webpack").HotModuleReplacementPlugin;
+const deps = require("../package.json").dependencies;
+
+module.exports = {
+  entry: path.resolve(__dirname, "..", "./src/index.ts"),
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: 'ts-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HotModuleReplacementPlugin(),
+    new ModuleFederationPlugin({
+      name: "app2",
+      library: { type: "var", name: "app2" },
+      filename: "remoteEntry.js",
+      exposes: {
+        "./App": "./src/App",
+      },
+      shared: {
+        ...deps,
+        react: {
+          eager: true,
+          import: "react", // the "react" package will be used a provided and fallback module
+          shareKey: "react", // under this name the shared module will be placed in the share scope
+          shareScope: "legacy", // share scope with this name will be used
+          singleton: true, // only a single version of the shared module is allowed
+        },
+        "react-dom": {
+          eager: true,
+          import: "react-dom", // the "react" package will be used a provided and fallback module
+          shareKey: "react-dom", // under this name the shared module will be placed in the share scope
+          shareScope: "legacy", // share scope with this name will be used
+          singleton: true, // only a single version of the shared module is allowed
+        },
+      },
+    }),
+    new HtmlWebpackPlugin({
+      title: "Hello Webpack bundled JavaScript Project",
+      template: path.resolve(__dirname, "..", "./src/index.html"),
+    }),
+  ],
+  output: {
+    path: path.resolve(__dirname, "..", "./dist"),
+    filename: "bundle.js",
+  },
+  devServer: {
+    port: 3001,
+    static: path.resolve(__dirname, "..", "./dist"),
+    hot: true,
+  },
+};
---
+++ packages/app2/src/App.tsx
@@ -0,0 +1,5 @@
+import * as React from "react";
+
+const App = () => <div>App 2</div>;
+
+export default App;
